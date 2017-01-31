3d0710333b0121e7db058ba3d015d30a22e851b1
John Rigby <john.rigby@vivint.com>
2017-01-31 14:23:15 -0700

    Add heredoc script

---
+++ scripting/01-heredoc/README.md
@@ -0,0 +1,1 @@
+Rewrite foo.sh to user a heredoc instead of a series of echo commands.
---
+++ scripting/01-heredoc/foo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+foo="bar"
+bar="foo"
+date=$(date)
+
+echo "foo is $foo"
+echo "bar is $bar"
+echo "date is $date"
